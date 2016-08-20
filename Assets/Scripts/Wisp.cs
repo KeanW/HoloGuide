c34883656bcb3f91959671800e3d3bd3d2ea932d
KeanW <kean.walmsley@autodesk.com>
2016-08-21 00:03:26 +0200

    Added gradual motion via slerp

--- Assets/Scripts/Wisp.cs
+++ Assets/Scripts/Wisp.cs
@@ -7,13 +7,21 @@ public class Wisp : MonoBehaviour
     Rotate rotate = null;
     AudioSource pop = null;
 
+    public float journeyTime = 1.0F;
+    private float startTime = 0F;
+
+    private Vector3 startPos;
+    private Vector3 endPos;
+
     void Start () 
 	{
         cam = this.GetComponentInParent<Camera>();
         sphere = GameObject.FindGameObjectsWithTag("Wisp")[0];
+
         if (sphere != null)
         {
             rotate = sphere.GetComponentInChildren<Rotate>();
+
             var sounds = sphere.GetComponents<AudioSource>();
             foreach (var sound in sounds)
             {
@@ -24,42 +32,63 @@ public class Wisp : MonoBehaviour
                 }
             }
         }
+        startTime = 0F;
 	}
 
 	void Update () 
 	{
         if (cam != null && sphere != null)
         {
-            var dist = Vector3.Distance(cam.transform.position, sphere.transform.position);
-            if (dist < 1)
+            if (startTime > 0F)
             {
-                if (pop != null)
-                    pop.Play();
-                if (sphere != null)
+                float fracComplete = (Time.time - startTime) / journeyTime;
+                sphere.transform.position = Vector3.Slerp(startPos, endPos, fracComplete);
+                if (Vector3.Distance(sphere.transform.position, endPos) < 0.0001F)
                 {
-                    bool hit1, hit2;
-                    Vector3 vec;
-                    var pos = sphere.transform.position;
-                    do
-                    {
-                        var half = new Vector3(pos.x, pos.y * 0.4f, pos.z);
-                        vec = new Vector3((Random.value - 0.5f) * 4f, 0f, (Random.value - 0.5f) * 4f);
-                        hit1 = Physics.Raycast(sphere.transform.position, vec, vec.magnitude * 2, SpatialMapping.PhysicsRaycastMask);
-                        hit2 = Physics.Raycast(half, vec, vec.magnitude * 2, SpatialMapping.PhysicsRaycastMask);
-                    } while (hit1 || hit2);
-                    sphere.transform.position = Vector3.Lerp(pos, pos + vec, 5f);
-                    //sphere.transform.Translate(vec);
+                    startTime = 0F;
                 }
-            }
-            else if (dist < 2)
-            {
-                if (rotate != null)
-                    rotate.speed = 6;
             }
             else
             {
-                if (rotate != null)
-                    rotate.speed = 3;
+                var dist = Vector3.Distance(cam.transform.position, sphere.transform.position);
+                if (dist < 1)
+                {
+                    if (pop != null)
+                        pop.Play();
+                    if (sphere != null)
+                    {
+                        startTime = Time.time;
+
+                        bool hit1, hit2;
+                        Vector3 vec;
+                        startPos = sphere.transform.position;
+                        do
+                        {
+                            var half = new Vector3(startPos.x, startPos.y * 0.4f, startPos.z);
+                            vec = new Vector3((Random.value - 0.5f) * 4f, 0f, (Random.value - 0.5f) * 4f);
+                            hit1 = Physics.Raycast(startPos, vec, vec.magnitude * 2, SpatialMapping.PhysicsRaycastMask);
+                            hit2 = Physics.Raycast(half, vec, vec.magnitude * 2, SpatialMapping.PhysicsRaycastMask);
+                        } while (hit1 || hit2);
+
+                        //sphere.transform.Translate(vec);
+
+                        endPos = startPos + vec;
+
+                        float fracComplete = (Time.time - startTime) / journeyTime;
+                        sphere.transform.position = Vector3.Slerp(startPos, endPos, fracComplete);
+                        //sphere.transform.position = Vector3.Lerp(startPos, endPos, fracComplete);
+                    }
+                }
+                else if (dist < 2)
+                {
+                    if (rotate != null)
+                        rotate.speed = 6;
+                }
+                else
+                {
+                    if (rotate != null)
+                        rotate.speed = 3;
+                }
             }
         }
 	}
