2c543d4de41e8b763c7d4fa93c63b0a6815db533
KeanW <kean.walmsley@autodesk.com>
2016-08-20 12:47:14 +0200

    Initial commit

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour 
+{
+	public float speed;
+
+	void Update () 
+	{
+		this.transform.Rotate(new Vector3(0,speed,0));
+	}
+}
---
+++ Assets/Scripts/Wisp.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Wisp : MonoBehaviour 
+{
+    Camera cam = null;
+    GameObject sphere = null;
+    Rotate rotate = null;
+    AudioSource pop = null;
+
+    void Start () 
+	{
+        cam = this.GetComponentInParent<Camera>();
+        sphere = GameObject.FindGameObjectsWithTag("Wisp")[0];
+        if (sphere != null)
+        {
+            rotate = sphere.GetComponentInChildren<Rotate>();
+            var sounds = sphere.GetComponents<AudioSource>();
+            foreach (var sound in sounds)
+            {
+                if (sound.clip.name.Contains("pop"))
+                {
+                    pop = sound;
+                    break;
+                }
+            }
+        }
+	}
+
+	void Update () 
+	{
+        if (cam != null && sphere != null)
+        {
+            if (Vector3.Distance(cam.transform.position, sphere.transform.position) < 1)
+            {
+                if (rotate != null)
+                    rotate.speed = 6;
+                if (pop != null)
+                    pop.Play();
+                if (sphere != null)
+                    sphere.transform.Translate((Random.value - 0.5f) * 4f, 0f, (Random.value - 0.5f) * 4f);
+            }
+            else
+            {
+                rotate.speed = 3;
+            }
+        }
+	}
+}
